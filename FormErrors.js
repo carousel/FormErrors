c6a8fcf9773e0359437bc16a8606fe2c037ce23c
Miroslav Trninic <miroslav.trninic@devlabs.ba>
2019-01-14 14:31:21 +0100

    Refactor

---
+++ FormErrors.js
@@ -0,0 +1,38 @@
+class FormErrors {
+    constructor() {
+        this.errors = {}
+    }
+
+    has(field) {
+        return this.errors.hasOwnProperty(field)
+    }
+
+    get(field) {
+        if (this.errors) {
+            if (this.errors[field]) {
+                return this.errors[field][0]
+            }
+        }
+    }
+
+    record(errors) {
+        this.errors = errors;
+    }
+
+    clear(field) {
+        if (this.errors) {
+            if (this.errors[field]) {
+                delete this.errors[field];
+                return;
+            }
+        }
+        this.errors = {}
+    }
+    any() {
+        if (this.errors) {
+            return Object.keys(this.errors).length > 0;
+        }
+    }
+}
+
+export default FormErrors;
--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-class FormErrors {
-    constructor() {
-        this.errors = {}
-    }
-    get(field) {
-        if (this.errors[field]) {
-            return this.errors[field][0];
-        }
-    }
-    record(errors) {
-        this.errors = errors;
-    }
-    clear(field) {
-        delete this.errors[field]
-    }
-    any() {
-        return Object.keys(this.errors).length > 0;
-    }
-}
-
-module.exports = FormErrors;
+require('Form');
+require('FormErrors');
